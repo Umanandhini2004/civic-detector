0e02fe8260c7ff1cc383b4adae794952f259244c
Umanandhini2004 <bumanandhini04@gmail.com>
2026-06-25 19:34:48 +0530

    Use MongoDB environment variable

--- backend/database.py
+++ backend/database.py
@@ -1,25 +1,4 @@
 from pymongo import MongoClient
-
-client = MongoClient(
-    "mongodb+srv://civic_user:Uma1207@cluster0.r4nryt6.mongodb.net/civic_db?retryWrites=true&w=majority",
-    serverSelectionTimeoutMS=30000,
-    connectTimeoutMS=30000
-)
-try:
-    print(client.admin.command("ping"))
-    print("Connected!")
-except Exception as e:
-    print("ERROR:", e)
-
-db = client["civic_db"]
-
-complaints = db["complaints"]
-users = db["users"]
-
-
-
-
-from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 
