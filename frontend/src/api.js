1115604d9da356455b70606ad8bd25233caded54
Umanandhini2004 <bumanandhini04@gmail.com>
2026-06-27 20:10:45 +0530

    Update frontend API URL for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000"
+  baseURL: "https://civic-detector-d1km.vercel.app"
 });
 
 API.interceptors.request.use((config) => {
